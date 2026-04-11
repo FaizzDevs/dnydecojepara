72166712f2cdf4ee29281fa739df1029228eeae4
FaizzDevs <faizdevs@gmail.com>
2026-04-11 11:26:52 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        domains: ['lh3.googleusercontent.com'],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
